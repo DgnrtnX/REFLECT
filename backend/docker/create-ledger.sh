3a0cc904417a6ff85d840a250c4363263de4355a
SIDD <siddgage@gmail.com>
2020-11-04 23:23:05 +0530

    hotfix-4

--- backend/docker/create-ledger.sh
+++ backend/docker/create-ledger.sh
@@ -1,37 +1,34 @@
 SYS_CHANNEL="sys-channel"
-CHANNEL_NAME="mychannel"
+CHANNEL_NAME="newChannel"
 
-chmod -R u+x ../../*
+chmod -R 0755 ../*
 
 # Delete existing artifacts
-rm -rf ../hyperledger/crypto-config/
+rm -rf ../hyperledger/crypto-config/*
 rm genesis.block $CHANNEL_NAME.tx
 rm -rf ../hyperledger/transactions/*
 
 #Generate Crypto artifactes for organizations
-cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
-
-echo "-----------------"
-echo "connecting " $CHANNEL_NAME
-echo "-----------------"
+cryptogen generate --config=./crypto-config.yml --output=../hyperledger/crypto-config/
 
 # Generate System Genesis block
 echo
-echo "generating genesis block"
+echo "----------  Generating genesis block  ----------"
 echo
 configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
 # Generate channel configuration block
 echo
-echo "generating channel transaction"
+echo "----------  Generating channel transaction  ----------"
+echo
 configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./$CHANNEL_NAME.tx -channelID $CHANNEL_NAME
 
 echo
 echo "----------  Generating anchor peer update for Org1MSP  ----------"
 echo
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ../hyperledger/transactions/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 
 echo
 echo "----------  Generating anchor peer update for Org2MSP  ----------"
 echo
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ../hyperledger/transactions/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
